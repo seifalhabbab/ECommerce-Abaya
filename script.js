563b162927f7d646b086d8047d61189be194fed9
Seif A. Al-Habbab <93404965+seifalhabbab@users.noreply.github.com>
2024-09-29 22:05:50 +0300

    Updated

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ document.addEventListener("click", (e) => {
 const packedLi = document.querySelectorAll(".packed");
 let packedList = document.querySelectorAll(".toggle-list");
 
-
 // packedLi.forEach((li) => {
 //   li.addEventListener("click", () => {
 //     packedList.forEach((list) => {
@@ -43,8 +42,28 @@ packedLi.forEach((li) => {
       if (list === e.currentTarget.querySelector(".toggle-list")) {
         list.classList.add("active");
       }
-    })
-  })
-})
+    });
+  });
+});
 
-// not clicking on a packed list should close it
+// ScrollReveal
+
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 1400,
+  reset: false,
+  mobile: true,
+});
+
+sr.reveal("header", { delay: 200, origin: "top" });
+sr.reveal(".main-title", { delay: 200, origin: "bottom" });
+sr.reveal("section.search", { delay: 200, origin: "top" });
+sr.reveal("section.landing", { delay: 200, origin: "top" });
+sr.reveal(".categories .product", { delay: 200, origin: "left" });
+sr.reveal(".categories .left", { delay: 200, origin: "right" });
+sr.reveal(".image-slide", { delay: 200, origin: "left" });
+sr.reveal(".newest .product", { delay: 200, origin: "bottom" });
+sr.reveal(".testimonials", { delay: 200, origin: "bottom" });
+sr.reveal(".services", { delay: 200, origin: "bottom" });
+sr.reveal(".links", { delay: 200, origin: "bottom" });
+// sr.reveal("footer", { delay: 200, origin: "bottom" });
